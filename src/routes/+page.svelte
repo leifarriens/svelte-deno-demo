a3678308e1cdc3bc8acb544ef99a6bbaa96e1c6b
Leif Arriens <leif.arriens@gmail.com>
2022-12-21 18:21:04 +0100

    Initial commit 🦕

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<main class="min-h-screen grid place-items-center bg-gradient-to-b from-teal-400 to-teal-600">
+	<section class="text-center text-white">
+		<h1 class="text-6xl font-bold mb-6">Welcome to SvelteKit running on Deno</h1>
+		<p class="text-xl">
+			Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+		</p>
+	</section>
+</main>
