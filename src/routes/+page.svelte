2c4416e4deaa53cfd172416d900a1a51f5c069de
Leif Arriens <leif.arriens@gmail.com>
2022-12-21 19:10:37 +0100

    add: more 🦕

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,14 @@
+<svelte:head>
+	<title>svelte-deno-demo</title>
+	<link
+		rel="icon"
+		href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🦕</text></svg>"
+	/>
+</svelte:head>
+
 <main class="min-h-screen grid place-items-center bg-gradient-to-b from-teal-400 to-teal-600">
-	<section class="text-center text-white">
-		<h1 class="text-6xl font-bold mb-6">Welcome to SvelteKit running on Deno</h1>
+	<section class="text-center text-white px-4">
+		<h1 class="text-6xl font-bold mb-6">Welcome to SvelteKit running on Deno 🦕</h1>
 		<p class="text-xl">
 			Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 		</p>
